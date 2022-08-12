156313cdf9ceda199dbf5aeb9bbbf22c9bd6b940
MarcosWinther <winthermarcos@gmail.com>
2022-08-11 23:11:17 -0300

    Class Calcultator implementado, futuramente mais implementaçõesdo script

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 class Calculator {
 
     constructor() {
-        this.upperValue = 0;
-        this.resultValue = 0;
+        this.upperValue = document.querySelector('#upper-number');
+        this.resultValue = document.querySelector('#result-number');
         this.reset = 0;
     }
+
+    btnPress() {
+        console.log(this);
+    }
+}
+
+// start Object Calculator
+let calc = new Calculator;
+
+console.log(calc);
+
+// start btns
+let buttons = document.querySelectorAll('.btn');
+
+console.log(buttons);
+
+// map all buttons
+for(let i = 0; buttons.length > i; i++) {
+    buttons[i].addEventListener('click', calc.btnPress);
 }
